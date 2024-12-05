593b1adb2107dd479a968e721798c3c13112a417
Manuel Ulate <fr3zs09@gmail.com>
2024-12-05 09:28:18 -0600

    feat:jwt on users

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { jwt } from 'hono/jwt';
+import type { JwtVariables } from 'hono/jwt';
+
 import UserHandler from './presentation/users/controllers/users.controller';
 
-const app = new Hono();
 
+
+
+type Variables = JwtVariables;
+const app = new Hono<{ Variables: Variables }>();
+
+app.use('/users/*', jwt({ secret: process.env.TOKEN_SECRET! }));
 app.route('/users', UserHandler );
 // app.route('/orders', );
 
--- src/presentation/users/controllers/users.controller.ts
+++ src/presentation/users/controllers/users.controller.ts
@@ -1,7 +1,7 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+
+
 import { users } from '../../../data/postgres';
-
-
 
 const app = new Hono();
 
